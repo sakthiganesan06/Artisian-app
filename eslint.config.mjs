ddeed5ab74d849154dc229a33787827da6f28234
Sakthi G <sakthi30116@gmail.com>
2026-09-01 01:07:15 +0530

    chore: clean eslint flat config and set ignoreDuringBuilds in next.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+export default [
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "node_modules/**",
+      "next-env.d.ts",
+      "*.config.js",
+      "*.config.mjs",
+    ],
+  },
+];
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
