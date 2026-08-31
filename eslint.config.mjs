b8604f97b0894c6201c1b5f607948e25aedc2c74
Sakthi G <sakthi30116@gmail.com>
2026-09-01 01:14:09 +0530

    chore: configure standard FlatCompat in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,16 @@
-import nextConfig from "eslint-config-next";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  ...nextConfig,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       ".next/**",
@@ -12,4 +21,7 @@ export default [
     ],
   },
 ];
+
+export default eslintConfig;
+
 
